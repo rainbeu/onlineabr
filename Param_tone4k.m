a4450bc97b57b292d85c4ad7b763cf3e27cedceb
surgery <surgery@star65.biologie.uni-oldenburg.de>
2014-09-05 15:41:28 +0200

    first commit star65

---
+++ Param_tone4k.m
@@ -0,0 +1,25 @@
+handles.Setup.Stimulus.Duration       = 0.001;
+handles.Setup.Stimulus.Type           = 'tone'; % 'tone', 'wave', 'click'
+handles.Setup.Stimulus.Frequency      = 8000;
+handles.Setup.Stimulus.RampDur        = 0.0005;
+handles.Setup.Stimulus.Window         = 'hann'; % 'none' or 'hann'
+handles.Setup.Stimulus.Level          = 70; % dB SPL after calibration
+
+handles.Setup.Stimulus.ITD            = 0 * 1e-6;  % sec.
+handles.Setup.Stimulus.ILD            = 0; % dB
+%handles.Setup.Stimulus.ILD            = 40:10:70; % dB
+handles.Setup.Stimulus.LevelThreshold = false; % for monaural level threshold: 
+                                               % set this to true, set
+                                               % level to 0, set ILD list
+                                               % to desired level steps
+
+handles.Setup.Recording.FileName    = 'data/datafile';
+
+handles.Setup.Recording.ExtraSmp    = 2400;
+handles.Setup.Recording.PreTime     = 0.004;
+handles.Setup.Recording.RecTime     = 0.035;
+
+handles.Setup.Recording.ArtefactThr = 20; % µV
+
+handles.Setup.Recording.MaxRepsPerCond = 500;
+% h0010-tones
---
+++ QuickCalibrator.m
@@ -0,0 +1,177 @@
+function [vfFullImpulseResponse] = QuickCalibrator(sSwitchSetting,sExpName,varargin)
+
+if ~exist('sExpName','var') || isempty(sExpName)
+    sExpName = '';
+end
+
+if ~exist('sSwitchSetting','var') || isempty(sSwitchSetting)
+    error('please use "low" or "-10", "mid" or "+4", and "high" or "Hi"');
+end
+
+
+nBufferLen = 8192;
+csPlotColorList = {'k','b','r','g'};
+
+%% get system settings from file
+if 2 ~= exist('system_settings')
+    error('please rename the file "system_settings_template.m" in the main exp_toolbox folder to "system_settings.m", and change settings where necessary');
+end
+stS = system_settings('quickcalibrator');
+
+fChirpAmp = stS.fChirpAmp;             
+fCalibrationSinusAmp = stS.fCalibrationSinusAmp;  
+fCalibrationFrequency = stS.fCalibrationFrequency;
+if length(varargin) >= 2 && strcmpi(varargin{2},'cross')
+    mfOutInChannelList = stS.mfCrossChannelList;         
+    sExpName = [sExpName '_crosstalk'];
+else
+    mfOutInChannelList = stS.mfOutInChannelList;         
+end
+vfExpFrequencyRange = stS.vfExpFrequencyRange;       
+nEqualFilterOrder = stS.nEqualFilterOrder;           
+nSamplingFrequency = stS.nSamplingFrequency;
+fSweepStartFrequency = stS.fSweepStartFrequency;
+fSweepRate = stS.fSweepRate;  
+if length(varargin) >= 1 && ~isempty(varargin{1}) 
+    nRepeats = varargin{1};
+else
+    nRepeats = stS.nRepeats;
+end
+sDeviceCode = stS.sDeviceCode;
+
+idx = find(strcmpi(sSwitchSetting,stS.MicrophoneCal(:,1)));
+if isempty(idx)
+    error('please use one of the following input settings: %s',sprintf('%s/',stS.MicrophoneCal{:,1}));
+else
+    fMicrophoneSensLevels = stS.MicrophoneCal{idx,2};
+end
+
+
+%% initialize
+
+if playrec('isInitialised')
+    playrec('reset');
+end
+
+vstSoundDevices = playrec('getDevices');
+stUsedDevice = vstSoundDevices(cellfun(@(x)~isempty(strfind(x,sDeviceCode)),{vstSoundDevices.name}));
+if isempty(stUsedDevice)
+    error('RME card not found');
+end
+if max(mfOutInChannelList(:,1)) > stUsedDevice.outputChans
+    error('output channel number mismatch');
+end
+if max(mfOutInChannelList(:,1)) > stUsedDevice.inputChans
+    error('input channel number mismatch');
+end
+
+playrec('init',nSamplingFrequency,stUsedDevice.deviceID,stUsedDevice.deviceID,max(mfOutInChannelList(:,1))+1,max(mfOutInChannelList(:,2))+1,nBufferLen);
+
+fChirpDuration = log(nSamplingFrequency/2/fSweepStartFrequency)/log(2^fSweepRate);
+vfChirpSignal = fChirpAmp           *[zeros(nSamplingFrequency/10,1);...
+                                      sin(2*pi*fSweepStartFrequency/log(2^fSweepRate)*(2.^(fSweepRate*(0:round(fChirpDuration*nSamplingFrequency)-1).'/nSamplingFrequency)-1));...
+                                      zeros(nSamplingFrequency/10,1)];
+vfSinusSignal = fCalibrationSinusAmp*[zeros(nSamplingFrequency/10,1);...
+                                      sin(2*pi*fCalibrationFrequency*(0:3*nSamplingFrequency-1).'/nSamplingFrequency);...
+                                      zeros(nSamplingFrequency/10,1)];
+bBP = fir1(1024,fCalibrationFrequency*2.^([-1 1]*1/6)/nSamplingFrequency*2);
+
+for nChannelIdx = 1:size(mfOutInChannelList,1)
+    
+    if length(fMicrophoneSensLevels) > mfOutInChannelList(nChannelIdx,2)+1
+        fMicrophoneSensLevel = fMicrophoneSensLevels(mfOutInChannelList(nChannelIdx,2)+1);
+    else
+        fMicrophoneSensLevel = fMicrophoneSensLevels(1);
+    end
+    fprintf('Microphone Sensitivity for Ch %1.0f: %1.2f\n',mfOutInChannelList(nChannelIdx,2)+1,fMicrophoneSensLevel);
+    
+    tic
+    nRecPage = playrec('playrec',repmat(vfChirpSignal,nRepeats,1),mfOutInChannelList(nChannelIdx,1)+1,nRepeats*length(vfChirpSignal),mfOutInChannelList(nChannelIdx,2)+1);
+    fWaitbarHandle = waitbar(0,'playing chirp signal');
+    while ~playrec('isFinished')
+        pause(0.1);
+        waitbar(toc*nSamplingFrequency/(nRepeats*length(vfChirpSignal)),fWaitbarHandle);
+    end
+    close(fWaitbarHandle);
+    vfRecordedSignal = mean(reshape(double(playrec('getRec',nRecPage)),[],nRepeats),2);
+    
+    vfFullImpulseResponse(:,nChannelIdx) = ifft(fft(vfRecordedSignal)./fft(vfChirpSignal));
+    if strcmpi(sSwitchSetting,'probe')
+        Tmp = fftfilt(stS.MicFilter(:,mfOutInChannelList(nChannelIdx,2)+1),[vfFullImpulseResponse(:,nChannelIdx);zeros(size(stS.MicFilter,1),1)]);
+        vfFullImpulseResponse(:,nChannelIdx) = Tmp(ceil(size(stS.MicFilter,1)/2)+(0:length(vfFullImpulseResponse)-1));
+    end
+    [dummy,nLatency] = max(abs(hilbert(vfFullImpulseResponse(1:nSamplingFrequency,nChannelIdx))));
+    
+    if nLatency > 4*nBufferLen
+        nLatency = 4*nBufferLen;
+    end
+    
+    nCutLength = 2^nextpow2(2*nLatency);
+    vfFrequencyAxis = (0:nCutLength-1).'/nCutLength*nSamplingFrequency;
+    figure(1)
+    subplot(size(mfOutInChannelList,1),1,nChannelIdx);
+    cla
+    for nDistortionOrder = 1:3;
+        vnDistortionPartIndex(:,nDistortionOrder) = round(-nSamplingFrequency*(log2(nDistortionOrder)/fSweepRate)) + (1:nCutLength).'; 
+        vnDistortionPartIndex(vnDistortionPartIndex(:,nDistortionOrder)<1,nDistortionOrder)=length(vfFullImpulseResponse(:,nChannelIdx))+vnDistortionPartIndex(vnDistortionPartIndex(:,nDistortionOrder)<1,nDistortionOrder);
+        mfSpectra(:,nDistortionOrder) = fft(hann(nCutLength).*vfFullImpulseResponse(vnDistortionPartIndex(:,nDistortionOrder),nChannelIdx));
+        vbPlotIdx = vfFrequencyAxis < nSamplingFrequency/2/nDistortionOrder;
+        semilogx(1/nDistortionOrder*vfFrequencyAxis(vbPlotIdx), db(abs(mfSpectra(vbPlotIdx,nDistortionOrder)))+fMicrophoneSensLevel,'color',csPlotColorList{nDistortionOrder});
+        hold on
+        grid on
+	if nDistortionOrder == 1
+	    vfShortIR = vfFullImpulseResponse(vnDistortionPartIndex(:,nDistortionOrder),nChannelIdx);
+	end
+    end
+    legend({'linear response','1st order harmonic','2nd order harmonic'},'location','northwest');
+    drawnow;
+
+    vfFilterAmplitudes = 1./abs(mfSpectra(:,1));
+    vfFilterAmplitudes = vfFilterAmplitudes/max(vfFilterAmplitudes(vfFrequencyAxis>vfExpFrequencyRange(1)&vfFrequencyAxis<vfExpFrequencyRange(2)));  
+    % new 2013-06-18
+    vfFilterAmplitudes = vfFilterAmplitudes / interp1(vfFrequencyAxis,vfFilterAmplitudes,fCalibrationFrequency);
+    vfFilterAmplitudes(vfFrequencyAxis<=vfExpFrequencyRange(1)|vfFrequencyAxis>=vfExpFrequencyRange(2)) = 1;
+    vfFilterAmplitudes(isnan(vfFilterAmplitudes)|isinf(vfFilterAmplitudes)) = 1;
+    mfOldFilterCoeffs(:,nChannelIdx) = fir2(nEqualFilterOrder,vfFrequencyAxis(vfFrequencyAxis<=nSamplingFrequency/2)/nSamplingFrequency*2,vfFilterAmplitudes(vfFrequencyAxis<=nSamplingFrequency/2));
+
+    mfFilterCoeffs(:,nChannelIdx) = arburg(vfShortIR,nEqualFilterOrder);
+    [H,F] = freqz(mfFilterCoeffs(:,nChannelIdx),1,2^16,nSamplingFrequency);
+    mfFilterCoeffs(:,nChannelIdx) = mfFilterCoeffs(:,nChannelIdx)/interp1(F,abs(H),fCalibrationFrequency);
+    
+    nRecPage = playrec('playrec',vfSinusSignal,mfOutInChannelList(nChannelIdx,1)+1,length(vfSinusSignal),mfOutInChannelList(nChannelIdx,2)+1);
+    fWaitbarHandle = waitbar(0,'playing sine tone');
+    while ~playrec('isFinished')
+        pause(0.1);
+        waitbar(toc*nSamplingFrequency/length(vfChirpSignal),fWaitbarHandle);
+    end
+    close(fWaitbarHandle);
+    vfRecordedSignal = playrec('getRec',nRecPage);
+    figure(2)
+    subplot(size(mfOutInChannelList,1),2,(nChannelIdx-1)*2+1);
+    plot(vfRecordedSignal);
+    vfRecordedSignal = fftfilt(bBP,[double(vfRecordedSignal);zeros(1024,size(vfRecordedSignal,2))]);
+    subplot(size(mfOutInChannelList,1),2,(nChannelIdx-1)*2+2);
+    plot(vfRecordedSignal);
+    
+    nStartSample = find(abs(vfRecordedSignal)>std(vfRecordedSignal),1,'first');
+    nStopSample  = find(abs(vfRecordedSignal)>std(vfRecordedSignal),1,'last');
+    
+        
+    vfMaxSPL(:,nChannelIdx) = db(std(vfRecordedSignal(nStartSample:nStopSample),1)/std(vfSinusSignal(nSamplingFrequency/10+1:end-nSamplingFrequency/10),1)) + fMicrophoneSensLevel;
+    
+    title(sprintf('output channel %u, input channel %u',mfOutInChannelList(nChannelIdx,:)));
+
+end
+
+save(sprintf('EqFiltCoeff_%s_%s.mat',sExpName,datestr(now,'yyyy-mm-dd')),'mfFilterCoeffs','nSamplingFrequency','mfOutInChannelList','vfMaxSPL','fCalibrationFrequency','fMicrophoneSensLevels');
+mfFilterCoeffs = mfOldFilterCoeffs;
+save(sprintf('EqFiltCoeff_%s_%s_DONOTUSEFORABR.mat',sExpName,datestr(now,'yyyy-mm-dd')),'mfFilterCoeffs','nSamplingFrequency','mfOutInChannelList','vfMaxSPL','fCalibrationFrequency','fMicrophoneSensLevels');
+save(sprintf('EqImpResp_%s_%s.mat',sExpName,datestr(now,'yyyy-mm-dd')),'vfFullImpulseResponse','nSamplingFrequency','mfOutInChannelList','vfMaxSPL','fCalibrationFrequency','fMicrophoneSensLevels');
+
+disp('The maximal output levels are:')
+for nChannelIdx = 1:size(mfOutInChannelList,1)
+    disp(sprintf('output channel: %u, maximum SPL: %5.1f',mfOutInChannelList(nChannelIdx,1),vfMaxSPL(nChannelIdx)));
+end
+
+playrec('reset');
+
