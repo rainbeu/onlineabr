d9e5eb64913a6122c68dc2fb421801110cee78c3
OP vorne Tierhaus <surgery@star65>
2018-06-18 12:42:56 +0200

    new P_* files

---
+++ P_all_thr_click.m
@@ -0,0 +1,28 @@
+handles.Setup.Stimulus.ITD = 0;  % (micro-)seconds
+handles.Setup.Stimulus.ILD = [70 80 90];%[40 50 60 70 80 90];  % level relative to Stimulus.Level in dB
+
+handles.Setup.Stimulus.Level = 0; % dB SPL after calibration (base value) (plus ILD values)
+
+handles.Setup.Recording.MaxRepsPerCond = 500;
+
+handles.Setup.Recording.ArtefactThr = 20;  % µV
+handles.Setup.Recording.RejectArtefacts = true;
+
+%%
+
+handles.Setup.Stimulus.Type = 'click';
+handles.Setup.Stimulus.Duration = 0.010;  % seconds
+handles.Setup.Stimulus.Window = 'none';
+handles.Setup.Stimulus.RampDur = 0.000;  % seconds
+
+%%
+
+handles.Setup.Stimulus.LevelThreshold = true; % !!!! important for level thresholds
+handles.Setup.Stimulus.PresentationType = 'L/R/B';  % measure all ears and binaural at once
+
+handles.Setup.Recording.ExtraSmp = 4800;
+handles.Setup.Recording.PreTime = 0.004;  % seconds
+handles.Setup.Recording.RecTime = 0.015;  % seconds
+
+handles.Setup.Stimulus.UseSignSwapping = true;
+
---
+++ P_bin_thr_click.m
@@ -0,0 +1,30 @@
+handles.Setup.Stimulus.StimulusLevelOffsets  = [40 50 60 70 80 90]; % dB re stimulus level ("Level")
+handles.Setup.Stimulus.StimulusSide =  'L+R';
+
+handles.Setup.Recording.MaxRepsPerCond = 500;
+handles.Setup.Recording.ArtefactThr = 20; % µV
+handles.Setup.Recording.RejectArtefacts = true;
+
+%%
+
+handles.Setup.Stimulus.Level          = 0; % dB SPL after calibration
+
+handles.Setup.Stimulus.Type           = 'click';
+handles.Setup.Stimulus.Duration       = 0.010;
+handles.Setup.Stimulus.Window         = 'none';
+handles.Setup.Stimulus.RampDur        = 0.000;
+
+handles.Setup.Stimulus.ITD            = [0] * 1e-6;  % sec.
+handles.Setup.Stimulus.ILD            = [0]; % dB
+
+%%
+
+handles.Setup.Stimulus.LevelThreshold = false;
+handles.Setup.Stimulus.UseSignSwapping = true;
+handles.Setup.Stimulus.PresentationType = 'simple binaural';
+
+handles.Setup.Recording.ExtraSmp    = 4800;
+handles.Setup.Recording.PreTime     = 0.004;
+handles.Setup.Recording.RecTime     = 0.015;
+
+
